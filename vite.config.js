b8c4055d95564e28b29e54030577230b42d7ec8c
Dario Veliz <velizalexander99@gmail.com>
2025-02-09 15:34:00 -0600

    out dist added

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist',  // Asegura que la salida sea en 'dist'
+    },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
-
             ],
             refresh: true,
-            manifest:true
+            manifest: true,
         }),
     ],
 });
+
+
